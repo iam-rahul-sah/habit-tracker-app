7c36de94280769f084ef931623ac7b974d702d4f
Rahul Sah <rahulsah0221@gmail.com>
2024-12-02 05:59:15 +0000

    added reducer for adding habit fixed use for display habit list using application state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store/store"
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/AddHabitForm";
+import HabitList from "./components/HabitList";
 
 function App() {
   return(
     <Provider store={store}>
-      <div>Hello world</div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">Habit Tracker</Typography>
+        <AddHabitForm/>
+        <HabitList/>
+      </Container>
     </Provider>
   )
 }
